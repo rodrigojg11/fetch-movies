b027c94a7422a9867be29b81049d04b115bbc220
Rodrigo Jaramillo <rodrigojaramilloga@gmail.com>
2025-05-13 17:15:25 -0500

    FetchMovies

--- fetch.js
+++ fetch.js
@@ -1,7 +1,7 @@
 import { arrayIteration } from "./index.js";
 
 export const fetchMovies2 = async (input) => {
-  const res = await fetch(`https://omdbapi.com/?apikey=f6da2807&s=${input}`)
+  const res = await fetch(`https://omdbapi.com/?apikey=${yourAPIKEY}=${input}`)
   const {Search} = await res.json()
   arrayIteration(Search);
 };
